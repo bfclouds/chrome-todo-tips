907fe0edfa730dd701a940579ef083d6a27de629
wenjiangf <f_wenjiang@163.com>
2021-09-28 11:40:17 +0800

    点完5分钟后提醒还会弹一此弹窗修复

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const NOTIFICATION_TEMPLATE_TYPE = {
   LIST: "list",
   PROGRESS: "progress"
 };
-const BASE_TIME = 6
-const ADD_BASE_INTERVAL_TIME = 300 // 5分钟
+const BASE_TIME = 20
+const ADD_BASE_INTERVAL_TIME =  300 // 5分钟
 
 const button1 = {
   title: '延迟5分钟提醒'
@@ -27,7 +27,9 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
   if (buttonIndex === 0) { // 延迟5分钟
+    clearTimeout(timer)
     intervalTime = ADD_BASE_INTERVAL_TIME
+    setTips()
   } else if(buttonIndex === 1) { // 已经写了
     isLock = true
   }
@@ -48,7 +50,7 @@ function tipTodo() {
 let date = new Date()
 
 function handleDate() {
-  date.setHours(18, 20, 0) // 时间设置为18:20:00
+  date.setHours(9, 20, 0) // 时间设置为18:20:00
   if ([3,4].includes(date.getDay())) {
     date.setHours(21, 20, 0) // 周三周四加班，时间设置为21:20:00
   }
@@ -66,9 +68,9 @@ function setTips () {
       isLock = false
     }
     // 超过5分钟提醒后重新按原来的时间间隔提醒
-    setTips()
     if (intervalTime !== BASE_TIME) {
       intervalTime = BASE_TIME
     }
+    setTips()
   }, 1000 * intervalTime)
 }
